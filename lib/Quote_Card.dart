ae3e7bdf10a45bdf40ae65e63ef7c17f05b4c2d0
RaviPabari <thakkarravi76@gmail.com>
2019-12-28 19:51:44 +0530

    Quotes app push

---
+++ lib/Quote_Card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+    final Quote quote;
+    QuoteCard({this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: 'Cookie',
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              quote.author,
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: 'Cookie',
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'Quote_Card.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomeScreen(),
+    ));
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<Quote> quotes = [
+    Quote(
+        author: '--Master Oogway',
+        text:
+            "Yesterday is history,tomorrow is a mystery,and today is a gift...that's why they call it present."),
+    Quote(
+        author: '--Master Shifu',
+        text:
+            "If you only do what you can do, you will never be more than who you are."),
+    Quote(author: "--Po's Father", text: "There is no secret ingredient."),
+    Quote(
+        author: "--Master Ooway",
+        text:
+            "Your mind is like this water, my friend. When it is agitated, it becomes difficult to see. But if you allow it to settle, the answer becomes clear"),
+   // Quote(
+     //   author: "--Master Shifu",
+       // text: "We do not wash our pits in the pool of sacred tears"),
+    Quote(
+        author: "--Po",
+        text:
+            "Legend tells of a legendary warrior whose kung fu skills were the stuff of legend."),
+    //Quote(
+        //author: "--Master Oogway",
+        //text: "There are no coincidences in this world."),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Quotes From Kung Fu Panda',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Cookie',
+            ),
+          ),
+          backgroundColor: Colors.grey,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.blueGrey,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.add,
+           // color: Colors.grey,
+          ),
+          backgroundColor: Colors.grey,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: quotes.map((quote) => QuoteCard(quote :quote)).toList(),
+        ));
+  }
+}
+
+
